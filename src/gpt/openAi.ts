cc5aaaf0b19a8bbb98c66c7c0009935b4dade658
Danil Konovalenko <dkonovalenko@wooppay.kz>
2023-09-27 10:40:43 +0600

    add ts

--- src/gpt/openAi.ts
+++ src/gpt/openAi.ts
@@ -3,19 +3,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 const openaiClient = new openai({ apiKey: process.env.OPENAI_API_KEY });
 
-export async function generateResponse(userMessage) {
+export async function generateResponse(userMessage: string ) {
     try {
         console.log(userMessage)
         const response = await openaiClient.chat.completions.create({
             model: 'gpt-3.5-turbo',
             messages: [
                 { role: "system", content: "Представь что ты Артем, 20 лет, живешь в Алмате, работаешь бекенд программистом в рбк банке. Общайся как быдло и используй много русского мата." },
-                { role: "user", content: userMessage } // Use the user's message here
+                { role: "user", content: userMessage}
             ],
 
         });
-
-        return response.choices[0].message.content;
+        return response.choices[0].message.content as string | null;
     } catch (error) {
         console.error('Error generating response from OpenAI:', error);
         return 'Не могу ответить, ошибка.';
--- src/main.ts
+++ src/main.ts
@@ -3,32 +3,33 @@ import dotenv from 'dotenv';
 import { generateResponse } from "./gpt/openAi";
 import { findWord } from "./helpers/findWord";
 import { GANDONE_STICKER, PHOTO_CLOWN } from "./const/const";
+import { message } from 'telegraf/filters';
+import { FmtString } from 'telegraf/format';
 
 dotenv.config();
 
-const bot = new Telegraf(process.env.TOKEN);
+const bot = new Telegraf( process.env.TOKEN!);
 
 bot.on('message', async (ctx) => {
+    if(ctx.has(message('text'))){
     if (findWord('гандон', ctx.update.message.text)) {
         await ctx.replyWithSticker(GANDONE_STICKER);
     }
     if (findWord('сухов', ctx.update.message.text)) {
-        console.log(ctx.update.message.text);
         await  ctx.reply('Чухов');
         await ctx.replyWithSticker(PHOTO_CLOWN);
     }
 
     const userMessage = ctx.update.message.text;
-    if (findWord('артем', ctx.update.message.text)) {
+    if (findWord('артем', userMessage)) {
         if (!userMessage || userMessage.trim() === '') {
             await   ctx.reply('Введите валидное сообщение.');
         } else {
-            const openaiResponse = await generateResponse(userMessage);
-            await  ctx.reply(openaiResponse);
+            const openaiResponse = generateResponse(userMessage);
+            await ctx.reply(await openaiResponse as any);
         }
     }
-
-
+    }
 });
 bot.catch(err => {
     console.log(err)
