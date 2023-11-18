885f640ee3acd410df155340114e4656e5a81ba2
honddk <honddk99@gmail.com>
2023-11-18 19:25:27 +0600

    add member leave reaction

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,41 @@
-import { Telegraf  } from 'telegraf';
+import { Telegraf } from 'telegraf';
 import dotenv from 'dotenv';
 import { generateResponse } from "./gpt/openAi";
 import { findWord } from "./helpers/findWord";
 import { GANDONE_STICKER } from "./const/const";
-import { message } from 'telegraf/filters';
 
 dotenv.config();
 const bot = new Telegraf( process.env.TOKEN!);
 
-bot.on('message', async (ctx: any) => {
-    if(ctx.has(message('text'))){
-    if (findWord('гандон', ctx.update.message.text)) {
-        await ctx.replyWithSticker(GANDONE_STICKER);
-    }
-
-
-    const userMessage = ctx.update.message.text;
-    if (findWord('бобр', userMessage)) {
-        if (!userMessage || userMessage.trim() === '') {
-            await ctx.reply('Введите валидное сообщение.');
-        } else {
-            const openaiResponse = generateResponse(userMessage);
-            await ctx.reply(await openaiResponse as any ,  {reply_to_message_id: ctx.message?.message_id});
+bot.on(['message', 'left_chat_member'], async (ctx: any) => {
+    if (ctx.message && ctx.message.text) {
+        if (findWord('гандон', ctx.message.text)) {
+            await ctx.replyWithSticker(GANDONE_STICKER);
         }
-    }
-    }
-    if (ctx.has(message('reply_to_message' ))){
-        if (ctx.message.reply_to_message && ctx.message.reply_to_message.from.id === ctx.botInfo.id) {
-            const userMessage = ctx.update.message.text;
+
+        const userMessage = ctx.message.text;
+        if (findWord('бобр', userMessage)) {
+            if (!userMessage || userMessage.trim() === '') {
+                await ctx.reply('Введите валидное сообщение.');
+            } else {
                 const openaiResponse = generateResponse(userMessage);
-                await ctx.reply(await openaiResponse as any ,  {reply_to_message_id: ctx.message?.message_id})
+                await ctx.reply(await openaiResponse as any, { reply_to_message_id: ctx.message.message_id });
+            }
         }
     }
 
-    if (ctx.filter('left_chat_member')) {
+    if (ctx.message && ctx.message.reply_to_message && ctx.message.reply_to_message.from.id === ctx.botInfo.id) {
+        const userMessage = ctx.message.text;
+        const openaiResponse = generateResponse(userMessage);
+        await ctx.reply(await openaiResponse as any, { reply_to_message_id: ctx.message.message_id });
+    }
+
+    if (ctx.message.left_chat_member) {
         const userWhoLeft = ctx.message.left_chat_member;
-        console.log('123', userWhoLeft);
-        ctx.reply(`Пользователь ${userWhoLeft.first_name} ${userWhoLeft.last_name} идет нахуй.`);
+        console.log(userWhoLeft);
+        await ctx.reply(`Пользователь ${userWhoLeft.first_name} ${userWhoLeft.last_name} идет нахуй.`);
     }
 });
-
-
 
 // bot.on('channel_post', (ctx: any) => {
 //     const channelPost = ctx.update.channel_post;
